0ab592b4af10a8cce6321d87c1f2c1175dc3124c
victormaofeng <3286408344@qq.com>
2020-08-13 17:50:12 +0800

    mao:test提交

--- src/main/java/com/dbsy/obe/ObeApplication.java
+++ src/main/java/com/dbsy/obe/ObeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class ObeApplication {
 
     public static void main(String[] args) {
